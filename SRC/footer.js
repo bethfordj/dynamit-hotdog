89e8b0bee074d60f2747e56ab05e0e95ec4d2508
Elizabeth Ford <daughterofspring@DaughteofSpring.attlocal.net>
2019-08-25 21:31:32 -0400

    files

---
+++ SRC/footer.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.getElementById("footer").innerHTML = `
+<footer>
+  <p class="copyright">® 2016 Dirty Dogs all rights reserved</p>
+  <div class="contact-info">
+    <p>274 Marconi Blvd. Columbus, Ohio 43215</p>
+    <p>614.538.0095</p>
+    <a id="footer-contact-link" href="/contact-us"><p>Contact Us</p></a>
+  </div>
+</footer>`;
